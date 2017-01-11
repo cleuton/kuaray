5c1f37aba6e67db53fe03211361eb4dbbc7d2a71
cleutonsampaio <cleuton.sampaio@gmail.com>
2017-01-11 14:34:50 -0200

    w1

--- authservice/app.js
+++ authservice/app.js
@@ -2,12 +2,54 @@
 var express = require('express');
 var app = express();
 var bodyParser = require("body-parser");
+var crypto = require('crypto');
+var base64 = require('base-64');
+
+//********************************
+var serverName = "MyTest01";
+var minutes = 1;
+//********************************
+/*
+  pending: 
+  a) Sign the hash with a private key;
+  b) Authenticate userName and password using a database or a file
+*/
+
+var tokenGenerator = function(username, ipAddress, serverName, loginDate, minutesValid) {
+  var token = {
+                'userName': username,
+                'loginDate': loginDate,
+                'originalIp': ipAddress,
+                'timeout': null,
+                'serverName': serverName,
+                'signature': null
+              };
+  var timeoutDate = new Date(loginDate.getTime() 
+    + minutesValid * 60000);
+  token.timeout = timeoutDate;
+  var tokenToSign = token.userName + ":" 
+                  + token.loginDate + ":"
+                  + token.originalIp + ":"
+                  + token.timeout + ":"
+                  + token.serverName;
+  var md5sum = crypto.createHash('md5');                  
+  md5sum.update(tokenToSign);                  
+  token.signature = md5sum.digest('hex');
+  tokenToSign += ":" + token.signature;
+  
+  return base64.encode(tokenToSign); 
+}
 
 app.use(bodyParser.json());
 
 app.post('/auth/token', function (req, res) {
-  console.log(req.body.username);
-  res.json({'test': false});
+  console.log(req.body.userName);
+  var token = tokenGenerator(req.body.userName, 
+                             req.connection.remoteAddress, 
+                             serverName,
+                             new Date(),
+                             minutes);
+  res.json({'token': token});
 });
 
 app.listen(3000, function () {
--- authservice/test/testCaseNormalLogin.js
+++ authservice/test/testCaseNormalLogin.js
@@ -2,12 +2,11 @@ var chakram = require('chakram'),
     expect = chakram.expect;
 
 describe('A valid user wants to get a valid token to access protected resources, and sends his “username” and “password”', function() {
-    it('Should return HTTP Status 200, a Header “location”, and an Entity like: {“token” : “<valor descrito acima>”>}', function() {
-        var user = {'username': 'cleuton', 'password': 'test'};
+    it('Should return HTTP Status 200, and an Entity like: {“token” : “<valor descrito acima>”>}; And the timeout period should be valid, against external parameter;', function() {
+        var user = {'userName': 'cleuton', 'password': 'test'};
         var options = {'headers' : {'Content-type': 'application/json'}};
         var response = chakram.post("http://localhost:3000/auth/token", user, options);
         expect(response).to.have.status(200);
-        expect(response).to.have.header('location');
         expect(response).to.have.json('token', function (token) {
             // Check whether the token is valid
             console.log("Token: " + token);
