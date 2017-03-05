a9e4fa3ef1978194f5e8c8e7620d0672feb1c5b9
Cleuton Sampaio <cleuton.melo@serpro.gov.br>
2017-03-05 11:18:21 -0300

    Finishing new server

--- kuarayserver/kuarayserver.js
+++ kuarayserver/kuarayserver.js
@@ -37,7 +37,7 @@ var receiveBuffer = new Buffer(sendBuffer.length);
 
 var httpServer = http.createServer(
     function(request,response) {
-        response.write(JSON.stringify(stats));
+        response.write(JSON.stringify(lastMeasure));
         response.end();
     }
 );
